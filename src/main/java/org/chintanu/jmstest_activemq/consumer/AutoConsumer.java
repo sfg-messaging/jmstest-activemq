67b28fddfde0ae39e40713f3db04de2879643ee7
ghostnsnow <ghostnsnow@users.noreply.github.com>
2024-11-07 20:52:09 +0530

    Send msg & receive a msga back(can be used as an ack)

--- src/main/java/org/chintanu/jmstest_activemq/consumer/AutoConsumer.java
+++ src/main/java/org/chintanu/jmstest_activemq/consumer/AutoConsumer.java
@@ -1,22 +1,42 @@
 package org.chintanu.jmstest_activemq.consumer;
 
+import jakarta.jms.JMSException;
 import jakarta.jms.Message;
+import lombok.RequiredArgsConstructor;
 import org.chintanu.jmstest_activemq.message.HelloWorldModel;
 import org.chintanu.jmstest_activemq.producer.AutoProducer;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
+@RequiredArgsConstructor
 public class AutoConsumer {
+
+    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = AutoProducer.SIMPLE_QUEUE)
     public void pop(@Payload HelloWorldModel model, @Headers MessageHeaders headers, Message message) {
 
-        System.out.println("Popped the queue now");
+        System.out.println("Popped the SIMPLE_QUEUE queue now");
         System.out.println(message);
         System.out.println(model);
+    }
+
+    @JmsListener(destination = AutoProducer.NOT_SO_SIMPLE_QUEUE)
+    public void popAndRespond(@Payload HelloWorldModel model, @Headers MessageHeaders headers, Message message) throws JMSException {
+
+        System.out.println("Popped the NOT_SO_SIMPLE_QUEUE queue now");
+        System.out.println(message);
+        System.out.println(model);
+
+        HelloWorldModel msg = HelloWorldModel.builder().msg("You sure you're Second?").uuid(UUID.randomUUID()).build();
+
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), msg);
     }
 }
--- src/main/java/org/chintanu/jmstest_activemq/producer/AutoProducer.java
+++ src/main/java/org/chintanu/jmstest_activemq/producer/AutoProducer.java
@@ -1,9 +1,15 @@
 package org.chintanu.jmstest_activemq.producer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.jms.JMSException;
+import jakarta.jms.Message;
+import jakarta.jms.Session;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.chintanu.jmstest_activemq.message.HelloWorldModel;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -15,9 +21,12 @@ import java.util.UUID;
 public class AutoProducer {
 
     public static final String SIMPLE_QUEUE = "convert.and.send";
-    private final JmsTemplate jmsTemplate;
+    public static final String NOT_SO_SIMPLE_QUEUE = "send.and.receive";
 
-    @Scheduled(fixedDelay = 3000)
+    private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
+
+    //@Scheduled(fixedDelay = 3000)
     public void push() {
 
         System.out.println("In push");
@@ -26,5 +35,33 @@ public class AutoProducer {
         jmsTemplate.convertAndSend(SIMPLE_QUEUE, msg);
 
         System.out.println("Pushed msg to topic : " + SIMPLE_QUEUE);
+    }
+
+    @Scheduled(fixedDelay = 3000)
+    public void pushWithReceiver() throws JMSException {
+
+        System.out.println("In pushWithReceiver");
+
+        HelloWorldModel msg = HelloWorldModel.builder().msg("Second msg Yay").uuid(UUID.randomUUID()).build();
+        Message receivedMSg = jmsTemplate.sendAndReceive(NOT_SO_SIMPLE_QUEUE, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+
+                Message message = null;
+                try {
+                    message = session.createTextMessage(objectMapper.writeValueAsString(msg));
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+                message.setStringProperty("_type", "org.chintanu.jmstest_activemq.message.HelloWorldModel");
+
+                System.out.println("Message constructed");
+
+                return message;
+            }
+        });
+
+        System.out.println("Got response back : " + receivedMSg.getBody(String.class));
     }
 }
